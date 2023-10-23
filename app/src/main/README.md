d5a2925eaf522017b23fd08353e024815558dfd9
Jan Rydzewski <jan.rydzewski1337@gmail.com>
2023-10-23 12:10:02 +0200

    zad 2

---
+++ app/src/main/README.md
@@ -0,0 +1,25 @@
+2023-10-23 11:19:43.672 31850-31850 MainActivity            com.example.zadanie1                 D  onCreate
+2023-10-23 11:19:43.816 31850-31850 MainActivity            com.example.zadanie1                 D  onStart
+2023-10-23 11:19:43.818 31850-31850 MainActivity            com.example.zadanie1                 D  onResume
+
+2023-10-23 11:22:01.521   400-494   goldfish-address-space  and...raphics.allocator@3.0-service  D  claimShared: Ask to claim region [0x3efd84000 0x3efda8000]
+2023-10-23 11:22:01.523   400-494   goldfish-address-space  and...raphics.allocator@3.0-service  D  claimShared: Ask to claim region [0x3efda8000 0x3efdcc000]
+2023-10-23 11:22:01.524   400-494   goldfish-address-space  and...raphics.allocator@3.0-service  D  claimShared: Ask to claim region [0x3efdcc000 0x3efdf0000]
+2023-10-23 11:22:01.526   924-1040  Parcel                  com.android.systemui                 W  Expecting binder but got null!
+2023-10-23 11:22:02.539   924-1040  EGL_emulation           com.android.systemui                 D  app_time_stats: avg=16.85ms min=10.91ms max=30.42ms count=60
+2023-10-23 11:22:02.768   420-544   TransactionTracing      surfaceflinger                       W  Could not find layer id -1
+2023-10-23 11:22:03.556   924-1040  EGL_emulation           com.android.systemui                 D  app_time_stats: avg=16.91ms min=15.64ms max=29.94ms count=60
+2023-10-23 11:22:03.675  9383-9437  BtGatt.ScanManager      com.android.bluetooth                D  uid 10115 isForeground true scanMode -1
+2023-10-23 11:22:03.675  9383-9437  BtGatt.ScanManager      com.android.bluetooth                D  uid 10115 isForeground true scanMode -1
+
+2023-10-23 11:26:43.162   626-650   AsyncRotation           system_server                        I  Async rotation timeout: {WindowToken{69d293f type=2024
+android.os.BinderProxy@ddcc15e}=com.android.server.wm.AsyncRotationController$Operation@7e387b8}
+2023-10-23 11:26:43.234   626-658   BroadcastQueue          system_server                        W  Background execution not allowed: receiving Intent { act=android.intent.action.DROPBOX_ENTRY_ADDED flg=0x10 (has extras) } to
+com.google.android.gms/.stats.service.DropBoxEntryAddedReceiver
+2023-10-23 11:26:43.234   626-658   BroadcastQueue          system_server                        W  Background execution not allowed: receiving Intent { act=android.intent.action.DROPBOX_ENTRY_ADDED flg=0x10 (has extras) } to
+com.google.android.gms/.chimera.GmsIntentOperationService$PersistentTrustedReceiver
+2023-10-23 11:26:43.446   626-650   system_server           system_server                        I  Explicit concurrent copying GC freed 284251(17MB) AllocSpace objects, 35(724KB) LOS objects, 50% free, 19MB/39MB, paused 263us,76us total
+91.961ms
+2023-10-23 11:26:43.585   420-544   TransactionTracing      surfaceflinger                       W  Could not find layer id -1
+2023-10-23 11:26:43.585   420-544   TransactionTracing      surfaceflinger                       W  Could not find layer id -1
+2023-10-23 11:26:43.585   420-544   TransactionTracing      surfaceflinger                       W  Could not find layer id -1
--- app/src/main/java/com/example/zadanie1/MainActivity.java
+++ app/src/main/java/com/example/zadanie1/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.zadanie1;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.core.view.WindowCompat;
@@ -26,7 +30,18 @@ public class MainActivity extends AppCompatActivity {
     private Button trueButton;
     private Button falseButton;
     private Button nextButton;
+    private Button answerButton;
     private TextView questionTextView;
+
+    private int currentIndex = 0;
+
+    private boolean answerWasShown;
+
+
+    private static final String KEY_CURRENT_INDEX = "currentIndex";
+    public static final String KEY_EXTRA_ANSWER = "com.example.zadanie1.MainActivity.correctAnswer";
+
+    private static final int REQUEST_CODE_PROMPT = 0;
 
     private Question[] questions = new Question[] {
             new Question(R.string.q_activity, true),
@@ -36,16 +51,23 @@ public class MainActivity extends AppCompatActivity {
             new Question(R.string.q_find_resources, false)
     };
 
-    private int currentIndex = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MainActivity", "onCreate");
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState != null) {
+            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
+        }
+
 
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
         nextButton = findViewById(R.id.next_button);
+        answerButton = findViewById(R.id.answer_button);
         questionTextView = findViewById(R.id.question_text_view);
 
         trueButton.setOnClickListener(new View.OnClickListener() {
@@ -66,9 +88,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 currentIndex = (currentIndex + 1) % questions.length;
+                answerWasShown = false;
                 setNextQuestion();
             }
         });
+
+        answerButton.setOnClickListener((v) -> {
+            Intent intent = new Intent(MainActivity.this, PromptActivity.class);
+            boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+            intent.putExtra(KEY_EXTRA_ANSWER, correctAnswer);
+            startActivityForResult(intent, REQUEST_CODE_PROMPT);
+        });
+
+
 
     }
 
@@ -79,11 +111,64 @@ public class MainActivity extends AppCompatActivity {
     private void checkAnswerCorrectness(boolean userAnswer) {
         boolean correctAnswer = questions[currentIndex].isTrueAnswer();
         int resultMessageId = 0;
-        if (userAnswer == correctAnswer)
-            resultMessageId = R.string.correct_answer;
-        else
-            resultMessageId = R.string.incorrect_answer;
+        if(answerWasShown) {
+            resultMessageId = R.string.answer_was_shown;
+        } else {
+            if (userAnswer == correctAnswer)
+                resultMessageId = R.string.correct_answer;
+            else
+                resultMessageId = R.string.incorrect_answer;
+        }
         Toast.makeText(this, resultMessageId, Toast.LENGTH_SHORT).show();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity", "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity", "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity", "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity", "onResume");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d("MainActivity", "Wywolana zostala metoda: onSaveInstanceState");
+        outState.putInt(KEY_CURRENT_INDEX, currentIndex);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode != RESULT_OK ) {return;}
+        if(requestCode == REQUEST_CODE_PROMPT) {
+            if(data == null) {
+                return;
+
+            }
+            answerWasShown = data.getBooleanExtra(PromptActivity.KEY_EXTRA_ANSWER_SHOWN, false);
+        }
+    }
 }
